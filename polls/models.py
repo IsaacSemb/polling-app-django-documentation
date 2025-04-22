13be1c8a852ec1364479a6ed2372f19bfa948690
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-22 23:30:00 +0100

    Notes on sample ORM manipulation and playing with the django shell

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.db import models
 
 from django.utils import timezone
@@ -12,8 +12,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
-    def was_published_recenetly(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
 
 
 
@@ -24,5 +24,33 @@ class Choice(models.Model):
     
     def __str__(self):
         return self.choice_text
-    
-    
+
+
+notes ="""
+
+from interactive shell
+using the API to play with the ORM 
+
+# creating a query set manager
+q = Question.objects.get(pk=1)
+
+# calling on all the choices for this question
+q.choice_set.all()
+
+# creating 3 answers for this question
+q.choice_set.create( choice_text = "I dont have one", votes = 0 )
+q.choice_set.create( choice_text = "Girl is my name", votes = 0 )
+c = q.choice_set.create( choice_text = "My name is a boy", votes = 0 )
+
+# following relationships till what you want
+Choice.objects.filter(question__pub_date__year=current_year)
+
+
+# Let's delete one of the choices. Use delete() for that.
+c = q.choice_set.filter(choice_text__startswith="Just hacking")
+c.delete()
+
+STUDY MORE ON ORM MANIPULATIONS
+
+
+"""
