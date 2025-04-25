cee8f439d0af8a32775c6ccdb489cc21440703be
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-25 03:16:58 +0100

    registering the views to the urls of the poll app

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path( "", views.index, name="index" )
+    path( "", views.index, name="index" ),
+    path( "<int:question_id>/", views.index, name="index" ),
+    path( "<int:question_id>/results", views.index, name="qtn_results" ),
+    path( "<int:question_id>/vote", views.index, name="qtn_vote" ),
+    
 ]
