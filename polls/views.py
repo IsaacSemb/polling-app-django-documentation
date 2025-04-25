6a8d402fde0257ea5f1c0b3181cdbc904be3c367
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-25 03:14:02 +0100

    Adding stubs to different views of a question

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,16 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, World. Youre at the polls index!")
+
+# seeing details on a question 
+def details(request, question_id):
+    return HttpResponse(f"Youre looking at question {question_id}")
+
+# seeing results of a question
+def results(request, question_id):
+    response = f"Youre looking at question {question_id}"
+    return HttpResponse(response)
+
+# voting on a question
+def vote(request, question_id):
+    return HttpResponse(f"Youre voting on question {question_id}")
