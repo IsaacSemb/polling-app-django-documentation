bc39bafefd15ce965a611bb69f45efac24e1cb7e
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-26 04:45:45 +0100

    Add logic to the index view to do something

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from polls.models import Question
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, World. Youre at the polls index!")
+    
+    # query latest 5 questions
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    
+    # create a string from all of them
+    out_put = ", ".join([q.question_text for q in latest_questions_list])
+    
+    
+    # return HttpResponse("Hello, World. Youre at the polls index!")
+    return HttpResponse(out_put)
 
 # seeing details on a question 
 def details(request, question_id):
