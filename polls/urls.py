3572ad1c773495f1bc188d98f76ad6bf023292bc
IsaacSemb <isaacsemb1996@gmail.com>
2025-04-26 07:50:31 +0100

    Experimenting with changed urls in new cleaner architecture

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
 
+# the name parameter is used to build the url using the url tag in templates
+# the 'name' value as called by the {% url %} template tag
+
 urlpatterns = [
     path( "", views.index, name="index" ),
-    path( "<int:question_id>/", views.details, name="qtn_details" ),
+    path( "<int:question_id>/details", views.details, name="qtn_details" ),
     path( "<int:question_id>/results/", views.results, name="qtn_results" ),
     path( "<int:question_id>/vote/", views.vote, name="qtn_vote" ),
     
