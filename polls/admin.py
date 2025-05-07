fceef7a3a2d7a2acdb2260d983bf4759710388c7
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-07 13:44:11 +0100

    Organize fields in admin edit page by category using fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -4,7 +4,13 @@ from .models import Question
 
 # create an admin for the model
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"] # flips the ordering when editting
+    # fields = ["pub_date", "question_text"] # flips the ordering when editting
+    
+    # more advanced thing to do is to use fieldsets
+    fieldsets = [
+        ( None, { "fields":[ "question_text" ] } ),
+        ( "Date Information", { "fields":[ "pub_date" ] } )
+    ]
     
 # link the admin and the model
 admin.site.register(Question, QuestionAdmin)
