91edd9b3ad0910fd5e0d28d87251137a05aaf716
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-07 13:21:32 +0100

    Create an admin for the question model and register its

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Question
 
-# Register your models here.
-admin.site.register(Question)
+
+# create an admin for the model
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"] # flips the ordering when editting
+    
+# link the admin and the model
+admin.site.register(Question, QuestionAdmin)
