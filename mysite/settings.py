36760f3861494eb83d03bfae7a3d72672bfb3d7f
IsaacSemb <isaacsemb1996@gmail.com>
2025-05-09 14:36:32 +0100

    Adding django debug toolbar

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     
     # third party apps
     'django_extensions',
+    'debug_toolbar',
     
     # project apps
     'polls.apps.PollsConfig', # you can just write 'polls' here instead of the whole path
@@ -49,12 +50,17 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = 'mysite.urls'
